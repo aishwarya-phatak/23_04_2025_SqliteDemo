43acb62248c4dd21785c87d6c06d73ebe16c8771
aishwarya <ashp17star@gmail.com>
2025-07-17 15:59:53 +0530

    .

--- 23_04_2025_SqliteDemo/DBHelper.swift
+++ 23_04_2025_SqliteDemo/DBHelper.swift
@@ -12,6 +12,7 @@ final class DBHelper {
     static let shared = DBHelper()
     var db : OpaquePointer?
     var dbPath = "mydatabase1.sqlite"
+    var employees : [Employee] = []
     
     init(){
         createDatabase()
@@ -56,5 +57,95 @@ final class DBHelper {
             print("Query Preparation is unsuccessful")
         }
         sqlite3_finalize(createStatement)
+    }
+    
+    func insertEmployeeRecords(empId : Int, empName : String){
+        let insertQueryString = "INSERT INTO EMPLOYEE(EmpId, EmpName) VALUES(?,?);"
+        
+        var insertStatement : OpaquePointer?
+        
+        if sqlite3_prepare_v2(db,
+                              insertQueryString,
+                              -1,
+                              &insertStatement,
+                              nil) == SQLITE_OK{
+            print("Insert Statement Prepapration is successful")
+            
+            sqlite3_bind_int(insertStatement,
+                             1,
+                             Int32(empId))
+            
+            sqlite3_bind_text(insertStatement,
+                              2,
+                              (empName as NSString).utf8String,
+                              -1,
+                              nil)
+            
+            if sqlite3_step(insertStatement) == SQLITE_DONE{
+                print("Employee Record inserted Successfully")
+            }else{
+                print("Employee rcord insertion unsuccessful")
+            }
+        } else {
+            print("Insert Statement Preparation is unsuccessful")
+        }
+        sqlite3_finalize(insertStatement)
+    }
+    
+    func retriveEmployeeRecords() -> [Employee]{
+        let retriveQuesryString = "SELECT * FROM EMPLOYEE;"
+        let retriveQueryString1 = "SELECT * FROM EMPLOYEE ORDER BY EmpId ASC;"
+        let retirveQueryString2 = "SELECT (EmpId,EmpName) FROM EMPLOYEE;"
+        let retriveQueryString3 = "SELECT * FROM EMPLOYEE ORDER BY EmpId DESC;"
+
+        var retriveStatement : OpaquePointer?
+        
+        if sqlite3_prepare_v2(db,
+                              retriveQuesryString,
+                              -1,
+                              &retriveStatement,
+                              nil) == SQLITE_OK{
+            print("Retrive Statement Prepapration is successful")
+            
+            while sqlite3_step(retriveStatement) == SQLITE_ROW{
+                let extractedEmpId = sqlite3_column_int(retriveStatement, 0)
+                let extractedEmpName = sqlite3_column_text(retriveStatement, 1)
+            
+                let extractedName : String = String(describing: String(cString: extractedEmpName!))
+                
+                let newEmployeeObject = Employee(empId: Int(extractedEmpId), empName: extractedName)
+                self.employees.append(newEmployeeObject)
+            }
+            
+        } else {
+            print("Retrive Statement Preparation Unsuccessful")
+        }
+        
+        return self.employees
+    }
+        
+    func deleteEmployeeRecords(empId : Int){
+        let deleteQueryString = "DELETE FROM EMPLOYEE where EmpId = ?;"
+        
+        var deleteStatement : OpaquePointer?
+        
+        if sqlite3_prepare_v2(db,
+                              deleteQueryString,
+                              -1,
+                              &deleteStatement,
+                              nil) == SQLITE_OK{
+            print("Delete Query Prepared Successfully")
+            
+            sqlite3_bind_int(deleteStatement, 1, Int32(empId))
+            
+            if sqlite3_step(deleteStatement) == SQLITE_DONE{
+                print("Employee record deleted successfully")
+            }else{
+                print("Employee recird deletion unsuccessful")
+            }
+        } else {
+            print("Delete Quesry Preparation unsucessful")
+        }
+        sqlite3_finalize(deleteStatement)
     }
 }
--- 23_04_2025_SqliteDemo/ViewController.swift
+++ 23_04_2025_SqliteDemo/ViewController.swift
@@ -12,7 +12,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        var dbHelper = DBHelper.shared
+        let dbHelper = DBHelper.shared
+//        dbHelper.insertEmployeeRecords(empId: 1007, empName: "Suchita")
+//        dbHelper.insertEmployeeRecords(empId: 1006, empName: "Saurabh")
+//        dbHelper.insertEmployeeRecords(empId: 1005, empName: "Heet")
+//        dbHelper.insertEmployeeRecords(empId: 1009, empName: "Prajakta")
+//        dbHelper.insertEmployeeRecords(empId: 1010, empName: "Krishna")
         
+           dbHelper.insertEmployeeRecords(empId: 1012, empName: "Sakshi")
+           dbHelper.insertEmployeeRecords(empId: 1016, empName: "Pooja")
+           dbHelper.insertEmployeeRecords(empId: 1015, empName: "Priyanka")
+           dbHelper.insertEmployeeRecords(empId: 1019, empName: "Shruti")
+           dbHelper.insertEmployeeRecords(empId: 1020, empName: "Akshay")
+        
+        for eachEmp in dbHelper.retriveEmployeeRecords(){
+            print("emp id is : \(eachEmp.empId) -- emp name is : \(eachEmp.empName)")
+        }
+        
+//        dbHelper.deleteEmployeeRecords(empId: 1007)
+        
+        for eachEmp in dbHelper.retriveEmployeeRecords(){
+            print("emp id is : \(eachEmp.empId) -- emp name is : \(eachEmp.empName)")
+        }
     }
 }
